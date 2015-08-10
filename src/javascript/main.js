59651dd3743af7b2b8710cccbaa243df9a782843
efurmantt <erik.furman@tradingtechnologies.com>
2015-08-10 11:41:40 -0500

    Remove randomData from bundle

--- src/javascript/main.js
+++ src/javascript/main.js
@@ -3,8 +3,6 @@ var _ = require('underscore');
 var grid = require('./components/grid.js');
 var GridModel = require('./components/gridmodel.js');
 
-window.randomData = require('./components/dummydata.js');
-
 if (!window.fin) {
     window.fin = {};
     window.fin.hypergridlite = {
